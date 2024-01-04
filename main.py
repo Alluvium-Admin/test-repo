1eee008618b3adafb375c9bc5827a47554731008
Yusuf Braimoh <73256563+Yusuf-TJ@users.noreply.github.com>
2024-01-04 13:17:04 +0100

    removed comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import unittest
 
-# Define a function to calculate the factorial of a number
 def factorial(n):
     if n < 0:
         raise ValueError("Factorial is not defined for negative numbers")
